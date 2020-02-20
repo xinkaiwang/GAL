b77588ce2d5a3c00b1193453e11bc954df04b671
Xinkai Wang <aakwxk@hotmail.com>
2020-02-19 22:36:22 -0800

    added more files, include full abel4w

---
+++ abel/ABEL4/lib.bat
@@ -0,0 +1,4 @@
+CD..
+CD Z:\src\GAL\ABEL4
+set Path = %Path%;Z:\src\GAL\abel4;
+abellib Z:\src\GAL\abel4\lib4\ABEL4LIB.DEV -e Z:\src\GAL P16V8S.dev >Z:\src\GAL\temp\lib.txt
---
+++ abel/temp/abel4bat.bat
@@ -0,0 +1,8 @@
+CD..
+CD Z:\src\GAL\ABEL4
+set Path = %Path%;Z:\src\GAL\abel4;
+ahdl2pla Z:\src\GAL\temp\dd.abl >Z:\src\GAL\temp\aa.txt
+plaopt.exe Z:\src\GAL\abel4\DS00.tt1 >Z:\src\GAL\temp\bb.txt
+fuseasm.exe -i Z:\src\GAL\abel4\DS00.tt2 >Z:\src\GAL\temp\cc.txt
+jedsim.exe Z:\src\GAL\abel4\DS00.jed Z:\src\GAL\abel4\DS00
+ :End
